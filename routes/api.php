4c29620f52010469f6f7519b8b80959200efc549
Tamara <tami_noeps@hotmail.com>
2020-04-22 18:01:20 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::apiResource('products', 'API\ProductController');
+//Route::apiResource('/products', 'API\ProductController');
+Route::get('/products', 'API\ProductController@index');
